e8292321afe3f4f511e52cc0b7686017f090f69d
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-15 07:03:27 +0100

    Add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable,
          jwt_revocation_strategy: self
 
+  enum role: { user: 0, admin: 1 }
+
+  after_initialize :set_default_role, if: :new_record?
+
   def jwt_payload
     super
   end
+
+  roles.keys.each do |role_name|
+    define_method("#{role_name}?") do
+      role == role_name
+    end
+  end
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_201820) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_223221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.bigint "city_id", null: false
+    t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["city_id"], name: "index_reservations_on_city_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,9 +48,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_201820) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti", null: false
+    t.string "name", null: false
+    t.string "username", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "cities"
+  add_foreign_key "reservations", "users"
 end
