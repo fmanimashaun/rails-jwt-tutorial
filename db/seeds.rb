208e3aba4c7b53bd1083e9ef25fbba639d613d03
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-15 07:38:50 +0100

    Add Rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 # create the admin user
-
-admin = User.find_or_create_by!(username: 'admin') do |user|
+User.find_or_create_by!(username: 'admin') do |user|
   user.password = 'password'
   user.password_confirmation = 'password'
   user.email = 'admin@example.com'
