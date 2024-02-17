cfeaa51fb4ac0710eb9373b51d0fedbcfe31cab5
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-17 09:50:37 +0100

    Fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,13 +7,15 @@ class Ability
     if user.admin?
       can :manage, :all
     else
+
+      # Non-admin users can only manage their own resources
       can :manage, :all
       cannot :manage, [Car, City]
       can %i[show], [Car]
 
       # Users can only update their own profile
       can :update, User, id: user.id
-     
+
       # Prevent non-admin users from updating their role
       cannot :update, User, role: 'admin'
 
