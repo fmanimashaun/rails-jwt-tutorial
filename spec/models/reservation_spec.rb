d404162dddea0b1b57179d87a407a51f51e07327
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-18 04:41:05 +0100

    Update test

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:reservation) { build(:reservation) }
+
+  it 'validates presence of date' do
+    reservation.date = nil
+    expect(reservation).not_to be_valid
+    expect(reservation.errors[:date]).to include("can't be blank")
+  end
+
+  it 'validates date cannot be in the past' do
+    reservation.date = Date.yesterday
+    expect(reservation).not_to be_valid
+    expect(reservation.errors[:date]).to include("can't be in the past")
+  end
+
+  it 'belongs to a city' do
+    expect(reservation).to respond_to(:city)
+  end
+
+  it 'belongs to a car' do
+    expect(reservation).to respond_to(:car)
+  end
+
+  it 'belongs to a user' do
+    expect(reservation).to respond_to(:user)
+  end
 end
