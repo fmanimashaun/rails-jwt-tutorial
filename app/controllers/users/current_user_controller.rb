d81ccd003d39718438dab9228f2889db2f72986f
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-03-04 19:08:14 +0100

    Update

--- app/controllers/users/current_user_controller.rb
+++ app/controllers/users/current_user_controller.rb
@@ -1,6 +1,13 @@
 class Users::CurrentUserController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+    render json: {
+      status: {
+        code: 200,
+        message: 'User details fetched successfully',
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }
+    }, status: :ok
   end
 end
